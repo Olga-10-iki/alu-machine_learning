216578fb2417b52f7b4abaf58f9973e1834329d4
Olga-10-iki <o.ikirezi1@alustudent.com>
2026-05-17 20:21:47 +0200

    Implement matrix multiplication function with numpy
    Adds a function to perform matrix multiplication using numpy.

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Perform matrix multiplication using numpy."""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Returns the matrix product of two numpy.ndarrays."""
+    return np.matmul(mat1, mat2)
